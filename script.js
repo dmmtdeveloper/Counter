fd6c5d015a9d05ded8dc61dad1951317693b7df7
David <125302548+dmmtdeveloper@users.noreply.github.com>
2023-09-22 12:46:17 -0300

    1

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ const counterValueElement = document.querySelector('.counter__value');
 
 resetButton.addEventListener('click', () => {
   //01 set counter value to 0
-  counterValueElement.textContent = 0; 
+  counterValueElement.textContent = 0;
 
   //02 Reset background color
   getCounter.classList.remove('counter--limit')
 
   //03 Reset counter title
-  getCounterTitle.innerHTML ='FANCY COUNTER'
+  getCounterTitle.innerHTML = 'FANCY COUNTER'
 
   //04 enabled increase and decrease buttons
   increaseButton.disabled = false;
@@ -29,13 +29,13 @@ decreaseButton.addEventListener('click', () => {
   const currentValue = counterValueElement.textContent
 
   //02 convert value to number type 
-  const currentValueAsNumber =  +currentValue
- 
+  const currentValueAsNumber = +currentValue
+
   //03 increment by 1  
   let newValue = currentValueAsNumber - 1;
 
   //04 check if new value is less than 0
-  if(newValue < 0){
+  if (newValue < 0) {
     newValue = 0;
   }
 
@@ -50,28 +50,28 @@ decreaseButton.addEventListener('click', () => {
 const incrementCounter = () => {
   //01 get current value of counter 
   const currentValue = counterValueElement.textContent;
-  
+
   //02 convert value to number type 
   const currentValueAsNumber = +currentValue;
-  
+
   //03 increment by 1 
   let newValue = currentValueAsNumber + 1;
 
   //04 check if new value is greater than 5
-  if(newValue > 5){
+  if (newValue > 5) {
     newValue = 5;
 
     //04.1 get a visual indicator
     getCounter.classList.add('counter--limit')
 
     //04.2 update counter Title
-    getCounterTitle.innerHTML ='Limit! Buy <b>Pro</b> for >5'
+    getCounterTitle.innerHTML = 'Limit! Buy <b>Pro</b> for >5'
 
     //04.3 disable increase and decrease buttons
     increaseButton.disabled = true;
     decreaseButton.disabled = true;
   }
-  
+
   //05 set counter element with new value 
   counterValueElement.textContent = newValue;
 
